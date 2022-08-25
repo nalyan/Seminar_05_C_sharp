af1cd89639666dae5f3b84eb004200a173d59700
ilnar <ilnar-sov@mail.ru>
2022-08-25 20:01:49 +0300

    Задачи с семинара

---
+++ Examples/Example_01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу копирования массива
+
+void CopyArray(int[] array, int[] newArray)
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        newArray[i] = array[i];
+    }
+}
+void PrintArray(int[] array)
+{
+        foreach (var element in array) Console.Write($"{element} "); 
+        Console.WriteLine();
+}
+
+Console.Clear();
+int[] array = {10, -54, 0, 489, -2, 86, 1};
+PrintArray(array);
+
+int[] newArray = new int[array.Length];
+CopyArray(array, newArray);
+PrintArray(newArray);
